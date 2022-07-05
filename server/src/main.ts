5dc7f2ad9fb4746a122efefddbb196733f8c492e
Zach Milne <zachmilne@Zachs-MacBook-Pro.local>
2022-07-05 17:10:06 -0400

    ensure downloads folder exists; upload button

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { mkdirSync, existsSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  if (!existsSync('./uploads')) mkdirSync('./uploads');
   const port = 3003;
   await app.listen(port);
   console.log('serving on port: ', port);
